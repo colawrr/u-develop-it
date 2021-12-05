4b846de6f1760f13ee6a655352accc58c49d07a5
Connor Lawrence <connor.lawrence96@gmail.com>
2021-12-05 15:43:03 -0700

    added voter

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -84,6 +84,26 @@ if (errors) {
       }
     });
   });
+
+  router.delete('/voter/:id', (req, res) => {
+    const sql = `DELETE FROM voters WHERE id = ?`;
+  
+    db.query(sql, req.params.id, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: res.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Voter not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+  });
   
 
   module.exports = router;
